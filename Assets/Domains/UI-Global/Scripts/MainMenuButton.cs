f0d07c32b1bd9081e3bdf89301360cbad5192545
Ben Shapiro <bshap93@gmail.com>
2025-05-08 15:06:33 -0600

    Main menu assurances

--- Assets/Domains/UI-Global/Scripts/MainMenuButton.cs
+++ Assets/Domains/UI-Global/Scripts/MainMenuButton.cs
@@ -1,26 +1,67 @@
 using Domains.Scene.MainMenu;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
+// Change this to use UnityEngine.UI
 
 namespace Domains.UI_Global.Scripts
 {
     public class MainMenuButton : MonoBehaviour
     {
+        // Make this public so it's easier to debug
+        public enum MainMenuButtonType
+        {
+            NewGame,
+            ContinueGame,
+            Settings,
+            QuitGame
+        }
+
         [SerializeField] private MainMenuButtonType buttonType;
 
         public bool asksForConfirmation;
-        [SerializeField] private Button button;
+        [SerializeField] private Button button; // This should now use UnityEngine.UI.Button
 
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        // Add debug logging to help diagnose the issue
+        private void Awake()
+        {
+            UnityEngine.Debug.Log($"MainMenuButton Awake: {buttonType}");
+
+            // Check if button component exists
+            if (button == null)
+            {
+                button = GetComponent<Button>();
+                if (button == null)
+                {
+                    UnityEngine.Debug.LogError($"No Button component found on {gameObject.name}");
+                    return;
+                }
+            }
+
+            // Add listener programmatically to ensure it's connected
+            button.onClick.AddListener(OnClick);
+        }
+
         private void Start()
         {
+            UnityEngine.Debug.Log($"MainMenuButton Start: {buttonType}, Has Save: {SceneLoadManager.HasGameSave()}");
+
             if (buttonType == MainMenuButtonType.ContinueGame)
-                if (SceneLoadManager.HasGameSave() == false)
-                    button.SetEnabled(false);
+            {
+                var hasSave = SceneLoadManager.HasGameSave();
+                UnityEngine.Debug.Log($"Continue button - HasSave: {hasSave}");
+
+                // Use button.interactable instead of SetEnabled
+                if (!hasSave)
+                    button.interactable = false;
+                else
+                    button.interactable = true;
+            }
         }
 
         public void OnClick()
         {
+            UnityEngine.Debug.Log($"MainMenuButton OnClick: {buttonType}");
+
             switch (buttonType)
             {
                 case MainMenuButtonType.NewGame:
@@ -32,6 +73,7 @@ namespace Domains.UI_Global.Scripts
                         MainMenuEvent.Trigger(MainMenuEventType.NewGameTriggered);
                     break;
                 case MainMenuButtonType.ContinueGame:
+                    UnityEngine.Debug.Log("Continue Game Button Clicked");
                     MainMenuEvent.Trigger(MainMenuEventType.ContinueGameTriggered);
                     break;
                 case MainMenuButtonType.Settings:
@@ -41,14 +83,6 @@ namespace Domains.UI_Global.Scripts
                     MainMenuEvent.Trigger(MainMenuEventType.QuitGameTriggered);
                     break;
             }
-        }
-
-        private enum MainMenuButtonType
-        {
-            NewGame,
-            ContinueGame,
-            Settings,
-            QuitGame
         }
     }
 }
